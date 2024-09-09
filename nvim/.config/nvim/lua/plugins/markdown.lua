b96f0ba8c6b3db954d1ef81fef4135a3be353db7
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-09-09 12:12:09 +0000

    removed atuin from zshrc, enabled zsh by defult

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,4 +1,4 @@
-{
+return {
     'MeanderingProgrammer/render-markdown.nvim',
     opts = {},
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
