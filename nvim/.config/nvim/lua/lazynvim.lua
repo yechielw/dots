63e4fcf72c750f84fd4d655d0154fc87eeeedf66
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-09-15 08:17:05 +0000

    fix: update gitignore, lazy.nvim config, and nix setup
    - Commented out lazy-lock.json in .gitignore to retain tracking.
    - Updated lazy.nvim setup to use a specific lockfile path.
    - Added nil_ls configuration in Neovim LSP settings.
    - Modified WSL NixOS configuration:
      - Added cargo package.
      - Introduced an activation script to run Lazy sync in headless mode.

--- nvim/.config/nvim/lua/lazynvim.lua
+++ nvim/.config/nvim/lua/lazynvim.lua
@@ -11,5 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup("plugins")
+require('lazy').setup("plugins", {
+  lockfile = vim.fn.expand("$HOME/dots/nvim/.config/nvim/lazy-lock.json")
+})
 
