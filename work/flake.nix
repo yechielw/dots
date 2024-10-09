9434926fb4f0577e91d749ddc5843a5223819a63
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-10-09 16:18:18 +0300

    added work profile

---
+++ work/flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags.url = "github:Aylur/ags";
+    
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+
+  };
+
+  outputs = { self, nixpkgs, nixos-cosmic, home-manager, ... }@inputs:
+
+  let
+    # ...
+	  system = "x86_64-linux"; # change to whatever your system should be.
+    pkgs = import nixpkgs {
+	    inherit system;
+	    overlays = [
+        inputs.hyprpanel.overlay
+	    ];
+	  };
+  in
+  {
+    nixosConfigurations.YECHIEL-THINKPAD = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        {
+          nix.settings = {
+            substituters = [
+              "https://cosmic.cachix.org/"
+              "https://hyprland.cachix.org"
+            ];
+            trusted-public-keys = [ 
+              "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+              "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            ];
+          };
+        }
+        nixos-cosmic.nixosModules.default
+        ./configuration.nix
+        home-manager.nixosModules.default
+      ];
+    };
+  };
+}
