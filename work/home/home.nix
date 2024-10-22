433b2a59adbbb23450e7e1c412fcdab301d2bd6d
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-10-22 15:29:15 +0300

    feat: Add new input sources and environment configurations
    - Introduced `fromYaml`, `gnome-shell`, `home-manager_2` sources in `flake.lock` for more streamlined dependency management.
    - Added new wallpaper setup and dark mode stylization via `stylix.nix`.
    - Updated `wm.nix` to include new system packages like nwg tools, wl-clipboard, waybar, and more for an improved Wayland experience.
    - Integrated additional tools such as `blueman`, `polkit_gnome`, and `brightnessctl`.
    - Modified `work.nix` to pass command line args for Microsoft Edge to enable Wayland Ozone support.

--- work/home/home.nix
+++ work/home/home.nix
@@ -1,34 +1,59 @@
-{ config, pkgs, ... }:
+{ config, pkgs,inputs, ... }:
 
 {
 
+  imports = [ 
+    #./hyprlnad.nix 
+    inputs.ags.homeManagerModules.default
+
+  ];
+  services.swaync.enable = true;
+  services.avizo.enable = true; 
+  services.kdeconnect.enable = true;
+
+
+  services.copyq.enable = true;
+  services.blueman-applet.enable = true;
+
+
+
+  programs.ags = {
+    enable = true;
+    extraPackages = with pkgs; [
+      gtksourceview
+      webkitgtk
+      accountsservice
+    ];
+  };
   programs.home-manager.enable = true;
   programs.zsh.enable = true;
-  
-  wayland.windowManager.hyprland = {
-    enable = false; 
-    settings = {
-      "$mod" = "SUPER";
-      bind =
-        [
-          "$mod, F, exec, firefox"
-          ", Print, exec, grimblast copy area"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (i:
-            let ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          )
-            9)
-        );
+  programs.nix-index.enable = true;
+
+  gtk = {
+    enable = true;
+    theme.name = "WhiteSur-Dark";
+    theme.package = pkgs.whitesur-gtk-theme;
+
+    font.name = "DejaVu Sans";
+    font.size = 11;
+
+    cursorTheme.name = "BreezeX-RosePine-Linux";
+    cursorTheme.package = pkgs.rose-pine-cursor;
+
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
     };
-  
   };
+
+  qt.enable = true;
+  qt.platformTheme.name = "gtk";
+  qt.style.name = "gtk2";
+
+  # stylix.enable = true;
+  
+
+
 
   home = {
     username = "yechiel";
--- work/wm.nix
+++ work/wm.nix
@@ -1,7 +1,57 @@
-{pkgs, ...}:
+{pkgs,inputs, ...}:
 
 {
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
   environment.systemPackages = with pkgs; [
     wofi
+    nwg-look
+    nwg-panel
+    nwg-drawer
+    nwg-displays
+    nwg-clipman
+    cliphist
+    wl-clipboard
+    polkit_gnome
+    kdePackages.qtwayland
+    brightnessctl
+    inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default
+    inputs.hyprpanel.packages.${pkgs.system}.default
+    inputs.quickshell.packages.${pkgs.system}.default
+    adw-gtk3
+    whitesur-gtk-theme
+    waybar
+    networkmanagerapplet
+    blueman
+    blueberry
+    ulauncher
+
+
+
+
+    #ags 
+    bun
+    dart-sass
+    swww
+    matugen
+    hyprpicker
+    slurp
+    grim
+    satty
+    wf-recorder
+    wl-clipboard
+    wayshot
+    swappy
+    asusctl
+    supergfxctl
+    libdbusmenu-gtk3
+
+
   ];
+
+
+
+  services.blueman.enable = true;
+
+
 }
