9131eabc2c92ed276bffee4c0eb44006a0bc8524
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-11-18 22:46:40 +0200

    zshrc  rewrite in nix

--- work/hacking.nix
+++ work/hacking.nix
@@ -1,14 +1,17 @@
 {pkgs,pkgs-master,inputs, ...}:
 
 {
+  programs.wireshark.enable = true;
   environment.systemPackages = with pkgs; [
     
     bloodhound-py
     caido
     netexec
     seclists
+    (pkgs.wordlists.override { lists = with pkgs; [ rockyou ]; })
     feroxbuster
     nmap
+    termshark
     wireshark
     metasploit
     apktool
---
+++ work/home/zsh.nix
@@ -0,0 +1,43 @@
+{ pkgs,inputs, settings, ... }:
+
+
+  {
+
+  programs = {
+    zsh = {
+      enable = true; 
+
+      enableCompletion = true;
+
+      #autosuggestion = true;
+
+      history.append = true; 
+      history.expireDuplicatesFirst = true;
+      history.save = 50000;
+      history.size = 50000;
+
+      autosuggestion = {
+        enable = true;
+        strategy = [ "history" "completion" ];
+        highlight = "fg=#999";
+      };
+      historySubstringSearch = {
+        enable = true;
+        searchDownKey = "$terminfo[kcud1]";
+        searchUpKey   = "$terminfo[kcuu1]";
+      }; 
+
+      antidote = {
+        enable = true;
+        plugins = [
+          #"zsh-users/zsh-history-substring-search"
+          "zdharma-continuum/fast-syntax-highlighting"
+          #"zsh-users/zsh-autosuggestions"
+          "zsh-users/zsh-completions"
+          #"olivierverdier/zsh-git-prompt"
+        ];
+      };
+    };
+  };
+
+}
