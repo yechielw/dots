95698a68e26f6140e050622f898a5c743484cd4b
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-11-19 11:56:27 +0200

    ohmyzsh

--- work/wm.nix
+++ work/wm.nix
@@ -1,6 +1,8 @@
 {pkgs,inputs, ...}:
 
 {
+  security.pam.services.gdm.enableGnomeKeyring = true;
+
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   environment.systemPackages = with pkgs; [
