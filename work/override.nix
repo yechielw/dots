17b9505d187708b04f9727b90683e2f3d02dbbcf
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-01-12 16:11:19 +0200

    feat: add custom systemd override and update configuration
    - Introduced `work/override.nix` to apply custom patches to the `systemd` package, including a patch for hibernate/resume functionality.
    - Updated `flake.nix` to include `./work/override.nix` in the `nixosConfigurations` imports.
    - Adjusted the background path in `work/home/home.nix` to point to a Google Drive location with a specified image.
    
    These changes enhance system customization and update configuration paths for consistency.

---
+++ work/override.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  systemd.package = pkgs.systemd.overrideAttrs (old: {
+    patches = old.patches ++ [
+      (pkgs.fetchurl {
+        url = "https://github.com/wrvsrx/systemd/compare/tag_fix-hibernate-resume%5E...tag_fix-hibernate-resume.patch";
+        hash = "sha256-Z784xysVUOYXCoTYJDRb3ppGiR8CgwY5CNV8jJSLOXU=";
+      })
+    ];
+  });
+}
