5ea1b48cd85cc3ef7322f7d8890c94c84c6b4bff
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-05-26 10:26:35 +0300

    enabled uwsm for better systemd integration

--- work/wm.nix
+++ work/wm.nix
@@ -28,6 +28,7 @@
       };
       hyprland = {
         enable = true;
+        withUWSM = true;
         package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
         portalPackage =
           inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
