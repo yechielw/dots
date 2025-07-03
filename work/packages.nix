c94243b0ad9ce0c8a05afa5abfea3cb4a7481354
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-07-03 18:30:47 +0300

    add nix updates to waybar

---
+++ work/packages.nix
@@ -0,0 +1,81 @@
+{
+  pkgs,
+  inputs,
+  pkgs-master,
+  ...
+}:
+{
+  environment.systemPackages = with pkgs; [
+    inputs.zen-browser.packages.${pkgs.system}.default
+    clamav
+    pkgs-master.bitwarden
+    nixd
+    alsa-utils
+    beeper
+    element-desktop
+    ghostty
+    obsidian
+    xxd
+    wirelesstools
+    qmk
+    via
+    bluez-tools
+    blueberry
+    espanso-wayland
+    nixfmt-rfc-style
+    dig
+    libmbim
+    copyq
+    (flameshot.override {
+      enableWlrSupport = true;
+      enableMonochromeIcon = true;
+    })
+    curl
+    gcc
+    file
+    git
+    go
+    google-chrome
+    gzip
+    kitty
+    neovim
+    lua-language-server
+    nodejs
+    pipx
+    pyenv
+    thunderbird
+    unzip
+    wget
+    killall
+    cargo
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    podman-desktop
+    docker-compose
+    podman-compose
+    distrobox
+    pciutils
+    libmbim
+    rquickshare
+    sbctl
+    trayscale
+    usbutils
+    cachix
+    element-desktop
+    jq
+    jqp
+    p7zip
+    uv
+    zed-editor
+    ddcutil
+    ddccontrol
+    python312Packages.impacket
+    (python3.withPackages (
+      ps: with ps; [
+        pynvim
+        pip
+        debugpy
+      ]
+    ))
+  ];
+}
--- work/wm.nix
+++ work/wm.nix
@@ -66,6 +66,7 @@
       grim
       satty
       wayshot
+      libnotify
     ];
 
     services = {
