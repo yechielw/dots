8b2993c8945164caa5c0ab78c3da707b371cf0e5
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-08-13 17:55:12 +0300

    systemd pached upstreamed

--- nix/override.nix
+++ nix/override.nix
@@ -5,12 +5,12 @@
   ...
 }:
 {
-  systemd.package = pkgs.systemd.overrideAttrs (old: {
-    patches = old.patches ++ [
-      (pkgs.fetchurl {
-        url = "https://github.com/wrvsrx/systemd/compare/tag_fix-hibernate-resume%5E...tag_fix-hibernate-resume.patch";
-        hash = "sha256-Z784xysVUOYXCoTYJDRb3ppGiR8CgwY5CNV8jJSLOXU=";
-      })
-    ];
-  });
+  # systemd.package = pkgs.systemd.overrideAttrs (old: {
+  #   patches = old.patches ++ [
+  #     (pkgs.fetchurl {
+  #       url = "https://github.com/wrvsrx/systemd/compare/tag_fix-hibernate-resume%5E...tag_fix-hibernate-resume.patch";
+  #       hash = "sha256-Z784xysVUOYXCoTYJDRb3ppGiR8CgwY5CNV8jJSLOXU=";
+  #     })
+  #   ];
+  # });
 }
