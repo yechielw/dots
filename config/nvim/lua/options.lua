1814a5c3670fb46b830e1b988018d236850efa3b
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-09-15 17:23:26 +0300

    nvim: fix nixd lsp expr, add popup round corner

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -8,6 +8,8 @@ vim.opt.relativenumber = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
+vim.opt.winborder = 'rounded'
+
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
