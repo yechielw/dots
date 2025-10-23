3920eae30c170c9d2a0e23bca125f54bf33874e5
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-10-24 02:12:42 +0300

    nvim(lspconfig): update lsp config to utilize nvim 0.11 lsp api

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,33 +1,7 @@
 return { -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
   dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for Neovim
-    {
-      'williamboman/mason.nvim',
-      -- NOTE: nixCats: use lazyAdd to only enable mason if nix wasnt involved.
-      -- because we will be using nix to download things instead.
-      enabled = require('nixCatsUtils').lazyAdd(true, false),
-      config = true,
-    }, -- NOTE: Must be loaded before dependants
-    {
-      'williamboman/mason-lspconfig.nvim',
-      -- NOTE: nixCats: use lazyAdd to only enable mason if nix wasnt involved.
-      -- because we will be using nix to download things instead.
-      enabled = require('nixCatsUtils').lazyAdd(true, false),
-    },
-    {
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-      -- NOTE: nixCats: use lazyAdd to only enable mason if nix wasnt involved.
-      -- because we will be using nix to download things instead.
-      enabled = require('nixCatsUtils').lazyAdd(true, false),
-    },
-
-    -- Useful status updates for LSP.
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim', opts = {} },
-
-    -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
     {
       'folke/lazydev.nvim',
       ft = 'lua',
@@ -38,38 +12,8 @@ return { -- LSP Configuration & Plugins
         },
       },
     },
-    -- kickstart.nvim was still on neodev. lazydev is the new version of neodev
   },
   config = function()
-    -- Brief aside: **What is LSP?**
-    --
-    -- LSP is an initialism you've probably heard, but might not understand what it is.
-    --
-    -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-    -- and language tooling communicate in a standardized fashion.
-    --
-    -- In general, you have a "server" which is some tool built to understand a particular
-    -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-    -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-    -- processes that communicate with some "client" - in this case, Neovim!
-    --
-    -- LSP provides Neovim with features like:
-    --  - Go to definition
-    --  - Find references
-    --  - Autocompletion
-    --  - Symbol Search
-    --  - and more!
-    --
-    -- Thus, Language Servers are external tools that must be installed separately from
-    -- Neovim. This is where `mason` and related plugins come into play.
-    --
-    -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-    -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-    --  This function gets run when an LSP attaches to a particular buffer.
-    --    That is to say, every time a new file is opened that is associated with
-    --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-    --    function will be executed to configure the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -182,76 +126,41 @@ return { -- LSP Configuration & Plugins
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     -- NOTE: nixCats: there is help in nixCats for lsps at `:h nixCats.LSPs` and also `:h nixCats.luaUtils`
     local servers = {}
-    -- servers.clangd = {},
-    servers.gopls = {
-      formatting = { gofumpt = true },
-    }
-    servers.pyright = {}
+
+    -- servers.pyright = {}
     -- servers.rust_analyzer = {},
-    servers.phpactor = {}
     -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
     --
     servers.csharp_ls = {}
-    -- Some languages (like typescript) have entire language plugins that can be useful:
-    --    https://github.com/pmizio/typescript-tools.nvim
     --
     -- But for many setups, the LSP (`tsserver`) will work just fine
     -- servers.tsserver = {},
     --
 
     -- NOTE: nixCats: nixd is not available on mason.
-    if require('nixCatsUtils').isNixCats then
-      require('lspconfig').nixd.setup {
-        cmd = { 'nixd' },
-        settings = {
-          nixd = {
-            nixpkgs = {
-              expr = '(builtins.getFlake "/home/yechiel/dots").inputs.nixpkgs { }',
+    vim.lsp.config('nixd', {
+      cmd = { 'nixd' },
+      settings = {
+        nixd = {
+          nixpkgs = {
+            expr = '(builtins.getFlake "/home/yechiel/dots").inputs.nixpkgs { }',
+          },
+          formatting = {
+            command = { 'nixfmt' },
+          },
+          options = {
+            nixos = {
+              expr = '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.lenovo-thinkpad-x13.options',
             },
-            formatting = {
-              command = { 'nixfmt' },
-            },
-            options = {
-              nixos = {
-                expr = '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.lenovo-thinkpad-x13.options',
-              },
-              home_manager = {
-                expr = '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.lenovo-thinkpad-x13.options.home-manager.users.type.getSubOptions []',
-              },
+            home_manager = {
+              expr = '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.lenovo-thinkpad-x13.options.home-manager.users.type.getSubOptions []',
             },
           },
         },
-      }
+      },
+    })
 
-      -- servers.nixd = {
-      --   options = {
-      --     home_manager = {
-      --       expr = '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.nixos.config.home-manager.users.yechiel',
-      --     },
-      --   },
-      -- }
-    else
-      servers.rnix = {}
-      servers.nil_ls = {}
-    end
-    -- servers.lua_ls = {
-    --   -- cmd = {...},
-    --   -- filetypes = { ...},
-    --   -- capabilities = {},
-    --   settings = {
-    --     Lua = {
-    --       completion = {
-    --         callSnippet = 'Replace',
-    --       },
-    --       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-    --       diagnostics = {
-    --         globals = { 'nixCats' },
-    --         disable = { 'missing-fields' },
-    --       },
-    --     },
-    --   },
-    -- }
-    require('lspconfig').lua_ls.setup {
+    vim.lsp.config('lua_ls', {
       settings = {
         capabilities = capabilities,
         Lua = {
@@ -260,56 +169,12 @@ return { -- LSP Configuration & Plugins
           },
           diagnostics = {
             globals = { 'nixCats' },
-            disable = { 'missing-fields' }, -- Suppresses missing field errors
+            disable = { 'missing-fields' },
           },
         },
       },
-    }
+    })
 
-    -- NOTE: nixCats: if nix, use lspconfig instead of mason
-    -- You could MAKE it work, using lspsAndRuntimeDeps and sharedLibraries in nixCats
-    -- but don't... its not worth it. Just add the lsp to lspsAndRuntimeDeps.
-    if require('nixCatsUtils').isNixCats then
-      for server_name, _ in pairs(servers) do
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-          cmd = (servers[server_name] or {}).cmd,
-          root_pattern = (servers[server_name] or {}).root_pattern,
-        }
-      end
-    else
-      -- NOTE: nixCats: and if no nix, do it the normal way
-
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
-      require('mason').setup()
-
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-    end
+    vim.lsp.enable { 'lua_ls', 'csharp_ls', 'nixd', 'pyright' }
   end,
 }
