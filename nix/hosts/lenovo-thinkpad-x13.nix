ea227680dfd8e008c06a2c9bb96f6acc6931ea96
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-10-23 18:29:05 +0300

    bluetooth driver tweaks

--- nix/hosts/lenovo-thinkpad-x13.nix
+++ nix/hosts/lenovo-thinkpad-x13.nix
@@ -38,7 +38,8 @@
 
     extraModprobeConfig = ''
       options btusb enable_autosuspend=N
-      options iwlwifi power_save=0
+      # options iwlwifi power_save=0
+      # options iwlwifi bt_coex_active=0
     '';
   };
 
@@ -60,6 +61,38 @@
     "d /var/lib/linux-enable-ir-emitter 0755 root root - -"
   ];
   environment.etc."linux-enable-ir-emitter".source = ../../config/linux-enable-ir-emitter;
+
+
+
+  services.udev.extraRules = ''
+    # AX211 Bluetooth
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0033", ATTR{authorized}="1"
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0033", ATTR{power/autosuspend}="-1"
+  '';
+  hardware.bluetooth = {
+    enable = true;
+
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        ControllerMode = "dual";
+        JustWorksRepairing = "confirm";
+        Privacy = "device";
+        # Enhanced timeout and retry settings for firmware loading
+        DiscoverableTimeout = 0;
+        PairableTimeout = 0;
+        AutoConnectTimeout = 60;
+        # Experimental features that may help with MT7925
+      };
+      Policy = {
+        AutoEnable = true;
+        ReconnectAttempts = 7;
+        ReconnectIntervals = "1,2,4,8,16,32,64";
+      };
+    };
+  };
+
+
 
   networking = {
     hostName = "lenovo-thinkpad-x13";
