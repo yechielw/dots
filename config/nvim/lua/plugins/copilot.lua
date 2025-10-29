e63a46ab834b4a9730b3637bd082a61432e128a9
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-10-29 23:27:14 +0200

    nvim: many nvim-cmp -> blink.cmp removed tabs indicator and ibl disabled confrom auto formating updated lsp to comply with new api

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,15 +1,20 @@
 return {
   {
     'zbirenbaum/copilot.lua',
-    config = function()
-      vim.g.copilot_enabled = 0
-      vim.g.copilot_worspace_folders = { '~/dots' }
-    end,
+    -- config = function()
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+    -- vim.g.copilot_enabled = 0
+    -- vim.g.copilot_worspace_folders = { '~/dots' }
+    -- end,
   },
   {
-    'zbirenbaum/copilot-cmp',
-    config = function()
-      require('copilot_cmp').setup()
-    end,
+    -- 'zbirenbaum/copilot-cmp',
+    -- config = function()
+    --   require('copilot_cmp').setup()
+    -- end,
+    'giuxtaposition/blink-cmp-copilot',
   },
 }
