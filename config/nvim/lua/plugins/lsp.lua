06d3b1d4f77e629c743419af071ef9601caf534a
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-11-10 11:26:29 +0200

    nvim: keydings

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -22,51 +22,51 @@ return { -- LSP Configuration & Plugins
         --
         -- In this case, we create a function that lets us more easily define mappings specific
         -- for LSP related items. It sets the mode, buffer and description for us each time.
-        local map = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
-
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-t>.
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-        -- Find references for the word under your cursor.
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-        -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-        -- Rename the variable under your cursor.
-        --  Most Language Servers support renaming across files, etc.
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-        -- Opens a popup that displays documentation about the word under your cursor
-        --  See `:help K` for why this keymap.
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header.
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
+        -- local map = function(keys, func, desc)
+        --   vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        -- end
+        --
+        -- -- Jump to the definition of the word under your cursor.
+        -- --  This is where a variable was first declared, or where a function is defined, etc.
+        -- --  To jump back, press <C-t>.
+        -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        --
+        -- -- Find references for the word under your cursor.
+        -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        --
+        -- -- Jump to the implementation of the word under your cursor.
+        -- --  Useful when your language has ways of declaring types without an actual implementation.
+        -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        --
+        -- -- Jump to the type of the word under your cursor.
+        -- --  Useful when you're not sure what type a variable is and you want to see
+        -- --  the definition of its *type*, not where it was *defined*.
+        -- map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        --
+        -- -- Fuzzy find all the symbols in your current document.
+        -- --  Symbols are things like variables, functions, types, etc.
+        -- map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+        --
+        -- -- Fuzzy find all the symbols in your current workspace.
+        -- --  Similar to document symbols, except searches over your entire project.
+        -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        --
+        -- -- Rename the variable under your cursor.
+        -- --  Most Language Servers support renaming across files, etc.
+        -- map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        --
+        -- -- Execute a code action, usually your cursor needs to be on top of an error
+        -- -- or a suggestion from your LSP for this to activate.
+        -- map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        --
+        -- -- Opens a popup that displays documentation about the word under your cursor
+        -- --  See `:help K` for why this keymap.
+        -- map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        --
+        -- -- WARN: This is not Goto Definition, this is Goto Declaration.
+        -- --  For example, in C this would take you to the header.
+        -- map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        --
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
@@ -101,9 +101,8 @@ return { -- LSP Configuration & Plugins
         --
         -- This may be unwanted, since they displace some of your code
         if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-          map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end, '[T]oggle Inlay [H]ints')
+          vim.keymap.set('n', '<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+          end, { buffer = event.buf, desc = '[T]oggle Inlay [H]ints' })
         end
       end,
     })
@@ -126,19 +125,20 @@ return { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     -- NOTE: nixCats: there is help in nixCats for lsps at `:h nixCats.LSPs` and also `:h nixCats.luaUtils`
-    local servers = {}
-
-    -- servers.pyright = {}
-    -- servers.rust_analyzer = {},
-    -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+    -- local servers = {}
     --
-    servers.csharp_ls = {}
+    -- -- servers.pyright = {}
+    -- -- servers.rust_analyzer = {},
+    -- -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+    -- --
+    -- servers.csharp_ls = {}
     --
     -- But for many setups, the LSP (`tsserver`) will work just fine
     -- servers.tsserver = {},
     --
 
-    -- NOTE: nixCats: nixd is not available on mason.
+    local nixopts = '(builtins.getFlake "' .. vim.env.HOME .. '/dots").nixosConfigurations.' .. vim.fn.hostname() .. '.options'
+
     vim.lsp.config('nixd', {
       cmd = { 'nixd' },
       settings = {
@@ -151,10 +151,11 @@ return { -- LSP Configuration & Plugins
           },
           options = {
             nixos = {
-              expr = '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.lenovo-thinkpad-x13.options',
+              expr = nixopts, -- '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.lenovo-thinkpad-x13.options',
             },
             home_manager = {
-              expr = '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.lenovo-thinkpad-x13.options.home-manager.users.type.getSubOptions []',
+              -- expr = '(builtins.getFlake "/home/yechiel/dots").nixosConfigurations.lenovo-thinkpad-x13.options.home-manager.users.type.getSubOptions []',
+              expr = nixopts .. '.home-manager.users.type.getSubOptions []',
             },
           },
         },
@@ -172,6 +173,7 @@ return { -- LSP Configuration & Plugins
             globals = { 'nixCats' },
             disable = { 'missing-fields' },
           },
+          hint = { enable = true },
         },
       },
     })
