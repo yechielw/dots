200ef224f0d6a8f977cced7506b0547d379c060f
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-11-25 18:48:26 +0200

    hypr: enable hyprsunset (blue light filtering

--- nix/modules/wm.nix
+++ nix/modules/wm.nix
@@ -95,6 +95,25 @@
         };
       };
 
+      hyprsunset = {
+        enable = true;
+        settings = {
+          max-gamma = 150;
+
+          profile = [
+            {
+              time = "7:30";
+              identity = true;
+            }
+            {
+              time = "21:00";
+              temperature = 5000;
+              gamma = 0.8;
+            }
+          ];
+        };
+      };
+
       hypridle = {
         enable = true;
 
