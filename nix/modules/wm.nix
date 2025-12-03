8c372382bd9a26c259f2e00c874244ba2e6b7c6d
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-12-03 10:02:35 +0200

    swaync: add declarative config

--- nix/modules/wm.nix
+++ nix/modules/wm.nix
@@ -58,6 +58,116 @@
         ];
       };
       swaync.enable = true;
+      swaync.settings = {
+        positionX = "right";
+        positionY = "top";
+        layer = "overlay";
+        control-center-layer = "top";
+        layer-shell = true;
+        cssPriority = "application";
+        control-center-margin-top = 0;
+        control-center-margin-bottom = 0;
+        control-center-margin-right = 0;
+        control-center-margin-left = 0;
+        notification-2fa-action = true;
+        notification-inline-replies = true;
+        notification-icon-size = 64;
+        notification-body-image-height = 100;
+        notification-body-image-width = 200;
+
+        widgets = [
+          "inhibitors"
+          "dnd"
+          "mpris"
+          "notifications"
+        ];
+        widget-config = {
+          notifications = {
+            vexpand = false;
+          };
+          inhibitors = {
+            text = "Inhibitors";
+            button-text = "Clear All";
+            clear-all-button = true;
+          };
+          title = {
+            text = "Notifications";
+            clear-all-button = false;
+            button-text = "Clear All";
+          };
+          dnd = {
+            text = "Do Not Disturb";
+          };
+          label = {
+            max-lines = 5;
+            text = "Label Text";
+          };
+          mpris = {
+            autohide = true;
+          };
+        };
+      };
+      swaync.style = ''
+                
+        :root {
+          --border-radius: 22px;
+          --cc-bg: transparent;
+
+          --widget-background: rgba(46, 46, 46, 0.7);
+          --noti-bg-alpha: 0.6;
+
+          --padding: calc(var(--border-radius) / 2);
+        }
+
+        .control-center {
+          border-radius: 0;
+        }
+
+        .widgets > .widget,
+        .widget-mpris > carouselindicatordots,
+        .widget-mpris > box > button {
+          background: var(--widget-background);
+          border-radius: var(--border-radius);
+          padding: calc(var(--border-radius) / 2);
+          border: var(--border);
+        }
+
+        .control-center-list-placeholder {
+          padding: var(--border-radius);
+        }
+
+        .notification-group {
+          border-radius: var(--border-radius);
+          padding: 8px;
+        }
+
+        .widget.widget-mpris {
+          background: transparent;
+          border-radius: 0;
+          padding: 0;
+          border: none;
+        }
+        .widget.widget-mpris > carouselindicatordots {
+          --dots-padding: 4px;
+          padding: var(--dots-padding);
+          padding-left: var(--dots-padding);
+          padding-right: calc(6px + var(--dots-padding));
+          margin: 0;
+          margin-top: var(--padding);
+        }
+        .widget-mpris > box > button:hover {
+          background: rgba(46, 46, 46, 1);
+        }
+        .widget-mpris-player {
+          box-shadow: none;
+          border: var(--border);
+          margin: 0 var(--padding);
+        }
+        .widget-mpris-player:only-child {
+          margin: 0;
+        }
+
+      '';
       swayosd = {
         enable = true;
         topMargin = 0.75;
