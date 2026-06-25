2d8a447edce5218b2c0b479ea2dfca35980e56ec
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2026-06-25 15:53:22 +0300

    add: taho theme

--- modules/home/appearance/default.nix
+++ modules/home/appearance/default.nix
@@ -4,43 +4,6 @@
   pkgs,
   ...
 }:
-let
-  tahoe = pkgs.stdenv.mkDerivation {
-    name = "macos-tahoe-theme";
-    version = "v0.6.2";
-    src = pkgs.fetchFromGitHub {
-      owner = "kayozxo";
-      repo = "GNOME-macOS-Tahoe";
-      tag = "v0.6.2";
-      sha256 = "sha256-Sp0ejlXy/cW6GFY0llD7JV5t1pWaHs7vG05ch7rF4Oo=";
-    };
-
-    nativeBuildInputs = with pkgs; [
-      dialog
-      glib
-      jdupes
-      libxml2
-      sassc
-      util-linux
-    ];
-
-    buildInputs = with pkgs; [
-      gnome-themes-extra
-    ];
-
-    dontBuild = true;
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/share/themes
-      cp -R gtk/Tahoe-Dark $out/share/themes
-      cp -R gtk/Tahoe-Light $out/share/themes
-
-      runHook postInstall
-    '';
-  };
-in
 {
   options.hm.appearance = {
     enable = lib.mkEnableOption "appearance";
@@ -51,17 +14,18 @@ in
       enable = true;
       force = true;
       recursive = true;
-      source = (tahoe + "/share/themes/Tahoe-Dark/gtk-4.0");
+      source = (pkgs.yechiel.tahoe + "/share/themes/MacTahoe-Dark/gtk-4.0");
     };
     gtk = {
       enable = true;
       theme = {
-        # name = "adw-gtk3-dark"; # "WhiteSur-Dark";
-        # package = pkgs.adw-gtk3; # pkgs.whitesur-gtk-theme;
-        name = "Tahoe-Dark";
-        package = tahoe;
+        name = "MacTahoe-Dark";
+        package = pkgs.yechiel.tahoe;
       };
-      gtk4.theme = null;
+      gtk4.theme = {
+        name = "MacTahoe-Dark";
+        package = pkgs.yechiel.tahoe;
+      };
 
       # iconTheme = {
       # name = "WhiteSur";
---
+++ packages/tahoe/default.nix
@@ -0,0 +1,48 @@
+{ lib, stdenvNoCC, fetchFromGitHub, sassc, glib, libxml2, gtk3, getent, which }:
+
+stdenvNoCC.mkDerivation {
+  pname = "mactahoe-gtk-theme";
+  version = "0-unstable-2026-02-20";
+
+  src = fetchFromGitHub {
+    owner = "vinceliuice";
+    repo = "MacTahoe-gtk-theme";
+    rev = "59fc8131e293bcf0c7a8eb55ba773cbcbcccb378";
+    hash = "sha256-xS/RAPAREzteA6BRL3ZGrKk8Uml6/AjZRGQGQCOCrek=";
+  };
+
+  nativeBuildInputs = [ sassc glib libxml2 gtk3 getent which ];
+
+  dontBuild = true;
+  dontFixup = true;
+
+  postPatch = ''
+    # The build sandbox has no real user in /etc/passwd; bypass getent lookup.
+    substituteInPlace libs/lib-core.sh \
+      --replace-fail 'MY_HOME=$(getent passwd "''${MY_USERNAME}" | cut -d: -f6)' \
+                     'MY_HOME="$HOME"' \
+      --replace-fail 'SUDO_BIN="$(which sudo)"' \
+                     'SUDO_BIN=""' \
+      --replace-fail 'if [[ ! -w "/root" ]]; then' \
+                     'if false; then'
+    patchShebangs install.sh libs/lib-install.sh libs/lib-core.sh
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/themes
+    HOME=$TMPDIR ./install.sh -l \
+      --dest $out/share/themes \
+      --silent-mode
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "MacOS Tahoe-like theme for GTK based desktops";
+    homepage = "https://github.com/vinceliuice/MacTahoe-gtk-theme";
+    license = licenses.gpl3Only;
+    platforms = platforms.unix;
+  };
+}
