e3020b4db4485094cbffe34332eab8564e7cab7d
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2026-09-10 12:08:52 +0300

    flake refactpr

--- flake.nix
+++ flake.nix
@@ -5,7 +5,16 @@
 
   inputs = {
 
-    flake-utils-plus.url = "github:Dines97/flake-utils-plus/aaf79700c35c2f1651843fc70fd104ce85b1171e";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
+
+    omniflake = {
+      url = "github:yechielw/omniflake/mine";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils-plus.url = "github:Dines97/flake-utils-plus";
 
     snowfall-lib = {
       url = "github:anntnzrb/snowfall-lib";
@@ -13,30 +22,22 @@
       inputs.flake-utils-plus.follows = "flake-utils-plus";
     };
 
-    omniflake = {
-      url = "github:yechielw/omniflake/mine";
+    raise = {
+      url = "github:yechielw/raise";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    beams.url = "github:kleinweb/beams";
-    beams.inputs.nixpkgs.follows = "nixpkgs";
-
-    # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
-    stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    raise.url = "github:yechielw/raise";
-    raise.inputs.nixpkgs.follows = "nixpkgs";
 
     profilepic = {
       url = "https://github.com/yechielw.png";
       flake = false;
     };
   };
-  outputs = inputs: import ./outputs.nix inputs;
+  outputs =
+    inputs:
+    import ./outputs.nix (
+      inputs
+      // {
+        home-manager = inputs.omniflake.flakes.home-manager;
+      }
+    );
 }
